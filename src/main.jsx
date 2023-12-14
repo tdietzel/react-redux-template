26c5ee368b49c5667dbaf6035838f265f6709218
Trent Dietzel <147360049+tdietzel@users.noreply.github.com>
2023-12-14 09:56:12 -0800

    Update typo

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App.jsx'
-import { Provider } from "react-redeux"
+import { Provider } from "react-redux"
 import { store } from "./redux/store.js"
 import './index.css'
 
